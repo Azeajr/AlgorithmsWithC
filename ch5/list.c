b3aace9b2657105ac22ae0b17c355ea059ed62b2
Antonio Zea Jr <halosprtn117@gmail.com>
2021-04-05 22:34:31 -0400

    Linked list implementation finish.

--- ch5/list.c
+++ ch5/list.c
@@ -12,6 +12,7 @@
 #include <string.h>
 
 #include "list.h"
+
 /**
  * @brief list_init
  * 
@@ -30,6 +31,7 @@ void list_init(List *list, void (*destroy)(void *data)){
 
     return;
 }
+
 /**
  * @brief list_destroy
  * 
@@ -43,7 +45,134 @@ void list_destroy(List *list){
      */
     while(list_size(list) > 0){
         if(list_rem_next(list, NULL, (void **)&data) == 0 && list->destroy != NULL){
+            /**
+             * @brief Call user-defined function to free dynamically allocated 
+             * data.
+             * 
+             */
             list->destroy(data);
         }
     }
+    /**
+     * @brief No operations are allowed now, but clear the structure as a 
+     * precaution.
+     * 
+     */
+    memset(list, 0, sizeof(List));
+
+    return;
+}
+
+/**
+ * @brief list_ins_next
+ * 
+ * @param list 
+ * @param element 
+ * @param data 
+ * @return int 
+ */
+int list_ins_next(List *list, ListElmt *element, const void *data){
+    ListElmt *new_element;
+    /**
+     * @brief Allocate storage for the element.
+     * 
+     */
+    if((new_element = (ListElmt *)malloc(sizeof(ListElmt))) == NULL){
+        return -1;
+    }
+    /**
+     * @brief Insert the element into the list.
+     * 
+     */
+    new_element->data = (void *)data;
+
+    if(element == NULL){
+        /**
+         * @brief Handle insertion at the head of the list.
+         * 
+         */
+        if(list_size(list) == 0){
+            list->tail = new_element;
+        }
+        new_element->next = list->head;
+        list->head = new_element;
+    } else {
+        /**
+         * @brief Handle insertion somewhere other than at the head.
+         * 
+         */
+        if(element->next == NULL){
+            list->tail =  new_element;
+        }
+        new_element->next = element->next;
+        element->next = new_element;
+    }
+    /**
+     * @brief Adjust the size of the list to account for the inserted elment.
+     * 
+     */
+    list->size++;
+    
+    return 0;
+}
+/**
+ * @brief list_rem_next
+ * 
+ * @param list 
+ * @param element 
+ * @param data 
+ * @return int 
+ */
+int list_rem_next(List *list, ListElmt *element, void **data){
+    ListElmt *old_element;
+    if(list_size(list) == 0){
+        return -1;
+    }
+    /**
+     * @brief Remove the element from the list.
+     * 
+     */
+    if(element == NULL){
+        /**
+         * @brief Handle removal from the head of the list.
+         * 
+         */
+        *data = list->head->data;
+        old_element = list->head;
+        list->head = list->head->next;
+
+        if(list_size(list) == 1){
+            list->tail = NULL;
+        }
+    } else {
+        /**
+         * @brief Handle removal form somewhere other than the head.
+         * 
+         */
+        if(element->next == NULL){
+            return -1;
+        }
+
+        *data = element->next->data;
+        old_element = element->next;
+        element->next = element->next->next;
+
+        if(element->next == NULL){
+            list->tail = element;
+        }
+    }
+
+    /**
+     * @brief Free the storage allocated by the abstract datatype.
+     * 
+     */
+    free(old_element);
+
+    /**
+     * @brief Adjust the size of the list to account for the removed element.
+     * 
+     */
+    list->size--;
+
+    return 0;
 }
