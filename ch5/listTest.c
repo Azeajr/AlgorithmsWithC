9464f633625347e53dca8a5c4f1b8c23138c26f7
Antonio Zea Jr <halosprtn117@gmail.com>
2021-04-06 16:43:58 -0400

    Draft

--- ch5/listTest.c
+++ ch5/listTest.c
@@ -32,6 +32,33 @@ int main(int argc, char const *argv[])
     List list;
     ListElmt *element;
 
+    int *data, i;
+
+    /**
+     * @brief Initialize the linked list.
+     * 
+     */
+    list_init(&list, free);
+
+    /**
+     * @brief Perform some linked list operations.
+     * 
+     */
+    element = list_head(&list);
+
+    for(i = 10; i > 0; i--){
+        if((data = (int *)malloc(sizeof(int))) == NULL){
+            return 1;
+        }
+
+        *data = i;
+
+        if(list_ins_next(&list, NULL, data) != 0){
+            return 1;
+        }
+    }
+
+
 
     return 0;
 }
